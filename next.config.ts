08c5c0db4e8352f5122c1de7d07768afeba801ec
NicolasYMonteiro <nicolasmonteiro0123@gmail.com>
2025-05-23 00:22:22 -0300

    chore: initial project structure organized by layout, features and components

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+import path from "path";
+
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@auth": path.resolve(__dirname, "src/features/auth"),
+      "@types": path.resolve(__dirname, "src/types"),
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
