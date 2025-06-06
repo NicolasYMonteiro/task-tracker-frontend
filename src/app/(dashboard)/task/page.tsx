a4626528f3481d3efc66cafd6126c0b1b6c5834e
NicolasYMonteiro <nicolasmonteiro0123@gmail.com>
2025-06-06 18:55:34 -0300

    fix: adjust layout for task display based on task count and improve empty state message

--- src/app/(dashboard)/task/page.tsx
+++ src/app/(dashboard)/task/page.tsx
@@ -27,12 +27,12 @@ const Page = () => {
     <div className="relative min-h-screen px-4 py-8">
       <div className="md:absolute inset-0 bg-cover bg-center bg-no-repeat opacity-25 pointer-events-none md:bg-[url('/greciaFundo.png')]" />
 
-      <div className="relative z-10 md:w-5/6 p-6 md:rounded-xl mx-auto">
+      <div className={ `relative z-10 ${tasks.length === 0 ? 'md:h-screen': 'md:h-auto'} md:w-5/6 p-6 md:rounded-xl mx-auto`}>
         <h1 className="text-4xl font-serif font-bold text-blue-900 text-center mb-6 tracking-wide">
           Tarefas do dia
         </h1>
 
-        <div className={`grid gap-6 grid-cols-1 ${tasks.length >= 7 ? 'md:grid-cols-2 xl:grid-cols-3' : 'md:grid-cols-2'}`}>
+        <div className={`grid gap-6 ${tasks.length === 0 ? 'md:h-2/5': 'md:h-auto'} grid-cols-1 ${tasks.length === 0 ? 'md:grid-cols-none xl:grid-cols-none' : 'md:grid-cols-2'} ${tasks.length >= 7 ? 'md:grid-cols-2 xl:grid-cols-3' : 'md:grid-cols-2'}`}>
 
           <div className="h-full w-full flex justify-center items-center">
             <CreateTaskModal />
@@ -43,7 +43,7 @@ const Page = () => {
               <TaskCard key={task.id} task={task} />
             ))
           ) : (
-            <div className="col-span-full text-center text-gray-500 italic">
+            <div className="col-span-full text-center mt-4 text-gray-500 italic">
               Nenhuma tarefa encontrada. Adicione uma tarefa para começar!
             </div>
           )}
