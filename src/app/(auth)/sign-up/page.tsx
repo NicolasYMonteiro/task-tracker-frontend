88395d5b733be89f42b4ae981170c447e70aac6e
NicolasYMonteiro <nicolasmonteiro0123@gmail.com>
2025-08-07 13:07:16 -0300

    refactor: remove img in login and register page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -4,16 +4,6 @@ import Image from 'next/image';
 const SignUp = () => {
     return (
         <section className="relative min-h-screen flex items-center justify-center px-2">
-            <div className="hidden md:block absolute inset-0 -z-10">
-                <Image
-                    src="/greciaFundo.png"
-                    alt="Templo Grego"
-                    fill
-                    className="object-cover"
-                    priority
-                />
-            </div>
-            <div className="md:absolute inset-0 bg-gradient-to-r from-black/15 via-black/5 to-black/15" />
             <SignUpForm />
 
         </section>
